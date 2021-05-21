bb17fb91db136b8c837471cc7fe9421ad0a74712
Ashwin <ashwinramachandrang@gmail.com>
2021-05-22 00:25:20 +0530

    pushed the ros files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/Documents/ITSP-2021/src
+# Build directory: /home/user/Documents/ITSP-2021/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("mrm_description")
